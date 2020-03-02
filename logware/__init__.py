ce8945a37f1ffc453ad87bbb54aba22f168cb723
Giridhar <samanth.giridhar@gmail.com>
2020-03-02 13:11:38 +0530

    Changes

--- logware/__init__.py
+++ logware/__init__.py
@@ -3,7 +3,7 @@ from webob import Request
 class RequestLogger:
 
     # Defaults
-    code_check_point = 100 
+    code_check_point = "1"
     service_name = None
     log_level = "info"
     encoding = 'utf-8'
@@ -18,19 +18,11 @@ class RequestLogger:
     def __call__(self,environ,start_response):
         request = Request(environ)
         def custom_start_response(status, headers, exc_info=None):
-            response = self.app(environ, start_response)
-            self.print_log(request,response,status)
-            return response
+            if status.startswith(self.code_check_point):
+                print('{"level":"'+self.log_level+'","service":"'+self.service_name+'","message":"","uri":"'+request.path_qs+'","responseCode":'+status.split(" ")[0]+',"requestId":"'+request.headers.get("request-id","")+'"}')
+            return start_response(status, headers, exc_info)
         return self.wrap_app(environ, custom_start_response)
 
     def set_code_check_point(self,log_level):
         if log_level == "error":
-            self.code_check_point = 400 
-
-    def print_log(self,request,response,status):
-        code = int(status.split(" ")[0])
-        if(code >= self.code_check_point):
-            message = ""
-            if response != None and len(response) > 0:
-                message = response[0].decode("utf-8").replace('"', '\\"') 
-            print('{"level":"'+self.log_level+'","service":"'+self.service_name+'","message":"'+message+'","uri":"'+request.path_qs+'","responseCode":'+str(code)+',"requestId":"'+request.headers.get("request-id","")+'"}')
+            self.code_check_point = "4"
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='logware',
-    version='0.1.2',
+    version='0.1.4',
     description='Logging middleware for python web services',
     url='https://github.com/Pratilipi-Labs/python-logware',
     author='Giridhar',
